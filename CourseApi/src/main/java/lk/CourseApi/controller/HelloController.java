1bb6ac755f6f64f899bca905a3ce4216cc73b112
Ann Sanchala Kavindi <annsanchala@gmail.com>
2026-07-07 12:23:34 +0530

    Welcome Part Added

--- CourseApi/src/main/java/lk/CourseApi/controller/HelloController.java
+++ CourseApi/src/main/java/lk/CourseApi/controller/HelloController.java
@@ -2,9 +2,21 @@ package lk.CourseApi.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 @RestController
 public class HelloController {
 
-    
+
+    @RequestMapping("/api/hello")
+    public String sayHello(){
+
+        return "Welcome to Course Portal";
+    }
+
+
+
+
     
 }
